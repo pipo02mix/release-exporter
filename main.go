b134310da711a44a9fb94e4d202326df4996047a
Fernando Ripoll <fernando@giantswarm.io>
2024-03-11 10:37:58 +0100

    Set a go timer to update releases every 30 min (#1)
    * Set a go timer to update releases every 30 min
    
    * git ignote
    
    * Use a ticket
    
    * Fire go routine before start teh server
    
    * Fire go routine before start teh server
    
    * Default value 0 to fail

--- main.go
+++ main.go
@@ -1,25 +1,39 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+var interval = flag.Int("run-every", 0, "The interval in minutes between updates")
+
 func main() {
+	flag.Parse()
+	if *interval <= 0 {
+		fmt.Println("The 'run-every' flag must be greater than 0.")
+		os.Exit(0)
+	}
+
 	releaseCollector, err := NewReleaseCollector()
 	if err != nil {
 		log.Fatal(err)
 	}
 	prometheus.MustRegister(releaseCollector)
 
+	ticker := time.NewTicker(time.Duration(*interval) * time.Minute)
+
 	go func() {
 		for {
+			fmt.Println("Updating entries from catalog.")
 			releaseCollector.UpdateFromCatalog()
-			time.Sleep(time.Minute * 30)
+			<-ticker.C
 		}
 	}()
 
